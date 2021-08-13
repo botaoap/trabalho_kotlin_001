78c02702127f38ecc63bb1a8dacd7ef6f2de0a77
botaoap <gabrielbotaoap@gmail.com>
2021-08-13 17:19:25 -0300

    Create two Activities

---
+++ trabalho_001/app/src/main/java/com/serasa/trabalho_001/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.serasa.trabalho_001.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.serasa.trabalho_001.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
